9f21bf2ec530b58e9b6e124bece80e0117bb6bc1
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2023-09-06 23:20:57 +0530

    added links

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
-  return <div className="fs-1">LogIn</div>;
+  return (
+    <div className="fs-1">
+      LogIn
+      <div>
+        <Link to={"/signup"} className="text-decoration-none">
+          signup
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
--- src/components/home/Navbar.js
+++ src/components/home/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="bg-color ">
       <header className="navbar  px-3 justify-content-between   navbar-expand-lg">
         <div>
-          <img height={80} src="/asessts/navbar/logo.png" alt="logo" />
+          <Link className="text-decoration-none" to={"/"}>
+            {" "}
+            <img height={80} src="/asessts/navbar/logo.png" alt="logo" />
+          </Link>
         </div>
 
         <div className="d-flex align-items-center gap-3 ">
@@ -14,7 +18,9 @@ const Navbar = () => {
           <FaShoppingCart style={{ color: "#E5D896", fontSize: "1.5rem" }} />
           <div className="d-flex align-items-center gap-2">
             <img height={20} src="/asessts/navbar/profile.png" alt="profile" />
-            <span style={{ color: "#E5D896" }}>LOGIN</span>
+            <Link className="text-decoration-none" to={"/login"}>
+              <span style={{ color: "#E5D896" }}>LOGIN</span>
+            </Link>
           </div>
 
           <img height={40} src="/asessts/navbar/crown.png" alt="crown" />
