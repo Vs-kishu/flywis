d93ab9a02388015adc63c2994564b6475fc4aeb6
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2023-09-07 09:15:52 +0530

    minor chnages

--- src/components/home/OfferCard.js
+++ src/components/home/OfferCard.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const OfferCard = () => {
   return (
-    <div className="d-flex align-items-center z-index btn-bg rounded-3  h-100 ">
-      <div className="w-50">
+    <div className="d-flex align-items-center justify-content-between px-3 z-index btn-bg rounded-3  h-100 ">
+      <div className="w-25">
         <img src="/asessts/specialOffer.png" className="img-fluid " alt="" />
       </div>
-      <div className="d-flex flex-column gap-5 py-2 ">
-        <h5 className="text-center fw-bolder">
+      <div className="d-flex flex-column gap-3 py-2 ">
+        <h5 className="text-center fs-6 fw-bolder">
           Get 50$ OFF on your First Visit{" "}
         </h5>
         <h5 className="pointer fw-bolder">{"CLICK TO VIEW MORE >"}</h5>
